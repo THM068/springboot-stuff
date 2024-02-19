ead5326f3cd5cbffd505fac220fdb955081efada
Thando Mafela <thando.mafela@sky.uk>
2024-02-19 22:11:15 +0000

    springsecurity_jpa

--- src/main/java/com/ht/managemybills/ManagemybillsApplication.java
+++ src/main/java/com/ht/managemybills/ManagemybillsApplication.java
@@ -1,9 +1,16 @@
 package com.ht.managemybills;
 
 import com.ht.managemybills.config.SocialMediaProperties;
+import com.ht.managemybills.entities.Post;
+import com.ht.managemybills.entities.User;
+import com.ht.managemybills.repositories.PostRepository;
+import com.ht.managemybills.repositories.UserRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 @EnableConfigurationProperties(SocialMediaProperties.class)
@@ -11,6 +18,20 @@ public class ManagemybillsApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(ManagemybillsApplication.class, args);
+  }
+
+  @Bean
+  CommandLineRunner commandLineRunner(PostRepository postRepository,
+      UserRepository userRepository, PasswordEncoder passwordEncoder) {
+    return  args -> {
+      var user = new User("user", passwordEncoder.encode("password"), "ROLE_ADMIN" );
+      var user1 = new User("username", passwordEncoder.encode("password"), "ROLE_ADMIN" );
+      var post = new Post("First Post", "This is the first post");
+      postRepository.save(post);
+
+      userRepository.save(user);
+      userRepository.save(user1);
+    };
   }
 
 }
